21d316589a339452ed495d12e6cab762d6ba1172
waninko <43968374+waninko@users.noreply.github.com>
2018-10-16 11:41:57 +0200

    exceptionsExample

---
+++ exceptionsExample/exceptionsExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptionsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person KjellArnt = new Person();
+
+            try
+            {
+               KjellArnt.fly(); 
+            }
+            catch(Exception feilmelding)
+            {
+                Console.WriteLine("Kjell-Arnt er ikke så flink til å fly.  ");
+                Console.WriteLine(feilmelding.Message);
+                
+                //Console.WriteLine("Men han skal ha for at han prøver der han flakser.. " +feilmelding.StackTrace);
+            }
+        }
+    }
+}
---
+++ exceptionsExample/exceptionsExample/person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptionsExample
+{
+    class Person
+    {
+        public void fly()
+        {
+           throw new ArgumentException("Han prøvde å fly, og hoppet fra en stol og gikk i bakken. Godt han ikke testa fra taket først."); 
+        }
+    }
+}
