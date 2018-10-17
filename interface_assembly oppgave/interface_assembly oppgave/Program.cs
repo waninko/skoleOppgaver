319c145c969590d4ad1298b2885ae99f164e39f7
waninko <43968374+waninko@users.noreply.github.com>
2018-10-17 11:26:10 +0200

    Interface/assembly POMODORO
    Oppgaven man inserter DLL'en og skriver ferdig program.

---
+++ interface_assembly oppgave/interface_assembly oppgave/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using PomodoroEngine;
+
+namespace interface_assembly_oppgave
+{
+    class Program : INotifyObject
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            Console.ReadLine(); //Hindre programmet fra å avslutte
+        }
+        public Program()
+        {
+            var pomodoro = new Pomodoro(this, 1);
+            pomodoro.Start();
+        }
+
+        public void Tick(int minutes, int seconds)
+        {
+            Console.Clear();
+            if (seconds == 0 && minutes == 0)
+            {
+                Console.WriteLine("Ta en pause!");
+                Environment.Exit(0);
+            }
+            Console.WriteLine(minutes + ":" + seconds.ToString("00"));
+
+        }
+    }
+}
