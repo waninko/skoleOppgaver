5b410bc835fa38248b30c4b86e2d479578356126
waninko <43968374+waninko@users.noreply.github.com>
2018-10-26 09:21:54 +0200

    SQLstart (fungererikkeutenDB)

---
+++ SQL_ADOnet/SQL_ADOnet/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace SQL_ADOnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //ADO.NET
+            var connectionString = ConfigurationManager.ConnectionStrings["sKursDB"].ConnectionString;
+           using(var connection = new SqlConnection(connectionString))
+            { 
+               connection.Open();
+            var command = connection.CreateCommand();
+            command.CommandText = " SELECT ID, Navn FROM Student";
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.Write("reader[0] = " + reader[0]+ " ");
+                Console.Write("reader[1] = " + reader[1] + " ");
+                Console.Write("reader[\"Id\"] = " + reader["Id"] + " ");
+                Console.Write("reader[\"Navn\"] = " + reader["Navn"] + " ");
+                Console.WriteLine();
+              }
+            }
+          
+            Console.ReadLine();
+        }
+    }
+}
