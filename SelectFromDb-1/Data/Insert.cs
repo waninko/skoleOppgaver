28dd88807d7b6d1761a119e48bbf679f7dba3ea6
waninko <43968374+waninko@users.noreply.github.com>
2018-11-01 13:18:53 +0100

    :|
    påbegynt uavhengigeCRUDs

---
+++ SelectFromDb-1/Data/Insert.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectFromDb.Data
+{
+   public class Insert<TDbModel> where TDbModel : new()
+    {
+        private PropertyInfo[] _properties;
+        private Type _dbModelType;
+
+        public Insert()
+        {
+             var dbModelType = typeof(TDbModel);
+            _properties = dbModelType.GetProperties();
+        }
+
+
+        public TDbModel InsertStudent(SqlConnection connection, string navn)
+        {
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "INSERT INTO Student (Navn) VALUES (@navn) ";
+                command.Parameters.AddWithValue("navn", navn);
+               // return AddToList(command).FirstOrDefault();
+            }
+
+        }
+
+      
+
+    }
+}
---
+++ SelectFromDb-1/Data/Update.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectFromDb.Data
+{
+   public class Update<TDbModel> where TDbModel : new()
+    {
+        private PropertyInfo[] _properties;
+        private Type _dbModelType;
+
+        public Update()
+        {
+            var dbModelType = typeof(TDbModel);
+            _properties = dbModelType.GetProperties();
+        }
+
+        public TDbModel UpdateStudent(SqlConnection connection, string navn)
+        {
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "UPDATE Student SET (Navn) = @navn WHERE @navn";
+                command.Parameters.AddWithValue("navn", navn);
+            }
+
+        }
+    }
+}
