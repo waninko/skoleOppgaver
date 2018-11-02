021c27ee971da5649c91738fd93c113a59d6d53d
waninko <43968374+waninko@users.noreply.github.com>
2018-11-02 15:25:38 +0100

    EntityFramework

---
+++ EntityFrameworkTest/EntityFrameworkTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using EntityFrameworkTest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new NorthwindContext();
+            var product = db.Products
+                .Include(p=> p.Category)
+                .ThenInclude(category => category.Products)
+                .Include(p => p.Supplier)
+                .First();
+            Console.WriteLine("Produkt: " + product.ProductName);
+            Console.WriteLine("Kategori: " + product.Category.CategoryName);
+            Console.WriteLine("Annet produkt i samme Kategori: " + product.Category.Products.Skip(4).First().ProductName);
+
+        }
+    }
+}
