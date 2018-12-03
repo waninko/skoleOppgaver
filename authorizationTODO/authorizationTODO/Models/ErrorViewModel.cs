261dd5c32d4693dbea7a12568e9a3a9a3363f587
waninko <43968374+waninko@users.noreply.github.com>
2018-12-03 15:17:03 +0100

    todo list w/authorization

---
+++ authorizationTODO/authorizationTODO/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace authorizationTODO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
