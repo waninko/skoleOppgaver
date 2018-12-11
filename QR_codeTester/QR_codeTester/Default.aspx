30d6659b91ede175e792798986c3b52a37b631f7
waninko <43968374+waninko@users.noreply.github.com>
2018-12-11 12:59:26 +0100

    Hide Link/Text in a QRcode
    Testing QR stuff! :D

---
+++ QR_codeTester/QR_codeTester/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtCode" runat="server"></asp:TextBox>
+            <asp:Button ID="btnGenerate" runat="server" Text="Generate QR" OnClick="btnGenerate_OnClick"/>
+            <br/>
+            <asp:PlaceHolder ID="PHQRCode" runat="server"></asp:PlaceHolder>
+            
+            </div>
+    </form>
+</body>
+</html>
---
+++ QR_codeTester/QR_codeTester/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using QRCoder;
+using Image = System.Web.UI.WebControls.Image;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnGenerate_OnClick(object sender, EventArgs e)
+    {
+        string Code = txtCode.Text;
+        QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        QRCodeGenerator.QRCode qrCode = qrGenerator.CreateQrCode(Code, QRCodeGenerator.ECCLevel.Q);
+
+        Image imgQrCode = new Image();
+        imgQrCode.Height = 150;
+        imgQrCode.Width = 150;
+
+        using (Bitmap bitmap = qrCode.GetGraphic(20))
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                byte[] byteImage = ms.ToArray();
+                imgQrCode.ImageUrl = "data:image/png;base64," + Convert.ToBase64String(byteImage);
+            }
+           PHQRCode.Controls.Add(imgQrCode); 
+        }
+
+    }
+}
