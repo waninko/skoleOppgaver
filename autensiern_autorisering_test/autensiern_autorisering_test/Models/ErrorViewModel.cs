77b0a9ee38163614b3a5f7d666d98a1e26982488
waninko <43968374+waninko@users.noreply.github.com>
2019-01-07 13:55:46 +0100

    -

---
+++ autensiern_autorisering_test/autensiern_autorisering_test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace autensiern_autorisering_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ node_testing/nodeApp/app.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var module1 = require('./module1');
+
+function onRequest(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.write(module1.myString);
+    module1.myFunction();
+    response.end();
+}
+
+http.createServer(onRequest).listen(8000);
