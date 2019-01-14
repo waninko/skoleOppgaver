659d9ba35e4a3e3a73e9690bbe5e453cfaa91552
waninko <43968374+waninko@users.noreply.github.com>
2019-01-14 13:39:49 +0100

    skjemaet funker! :D

--- form.php
+++ form.php
@@ -4,60 +4,45 @@
   <title>PHP CRUDs</title>
 </head>
 <body>
-<? PHP
-  $name = '':
-  $gender = '';
-  $color = '';
+<?php 
+	if (isset($_POST['submit']))
+	{
+		$ok =true;
+		if (!isset($_POST['name']) || $_POST['name'] === '' ){
+			$ok = false;
+		}
 
-  if(isset($_POST['submit'])){
-      $ok = true;
-      if(!isset($_POST['name']) || $_POST['name'] === '' ){
-        $ok = false;
-      } else{
-          $name = $_POST['name'];
-      }
+		if (!isset($_POST['gender']) || $_POST['gender'] === '' ){
+			$ok = false;
+		}
 
-      if(!isset($_POST['gender']) || $_POST['gender'] === '' ){
-        $ok = false;
-      } else{
-          $gender = $_POST['gender'];
-      }
+		if (!isset($_POST['color']) || $_POST['color'] === '' ){
+			$ok = false;
+		}
 
-      if(!isset($_POST['color']) || $_POST['color'] === '' ){
-        $ok = false;
-      } else{
-          $color = $_POST['color'];
-      }
 
-      if ($ok){
-      printf('User name: %s
-      <br>Gender: %s
-      <br>Color: %s',
-      htmlspecialchars($_POST['name']),
-      htmlspecialchars($_POST['gender']),
-      htmlspecialchars($_POST['color']);
-   }
-  }
+		if ($ok){
+		printf('User name: %s
+			  <br>Gender: %s
+			  <br>Color: %s',
+			 htmlspecialchars($_POST['name']),
+			 htmlspecialchars($_POST['gender']),
+			 htmlspecialchars($_POST['color']));
+		}
+	}
 ?>
-<form method="post" action="">
-  User name: <input type="text" name="name" value="<?php
-    echo htmlspecialchars($name);
-  ?>"> <br>
+
+
+<form method="post" action=""> <hr>
+  User name: <input type="text" name="name"> <br>
   
   Gender:
-    <input type="radio" name="gender" value="f" <?php
-      if ($gender === 'f'){
-      echo ' checked';
-      }
-    ?>>female
-    <input type="radio" name="gender" value="m" <?php
-      if ($gender === 'm'){
-      echo ' checked';
-      }
-    ?>>male
-
+    <input type="radio" name="gender" value="f"> female
+    <input type="radio" name="gender" value="m"> male
+	<br>
   Favorite color:
       <select name="color">
+		  <option value=" ">PLEASE SELECT</option>
           <option value="#f00">red</option>
           <option value="#0f0">green</option>
           <option value="#00f">blue</option>
