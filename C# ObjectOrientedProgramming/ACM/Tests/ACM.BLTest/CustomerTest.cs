737fe1893161670263fd793dab081dd9096d8d62
waninko <43968374+waninko@users.noreply.github.com>
2019-05-22 14:42:16 +0200

    Objects, classes & my first unitTest!

---
+++ C# ObjectOrientedProgramming/ACM/Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Ted",
+                LastName = "Tysne"
+            };
+            string expected = "Tysne, Ted";
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                LastName = "Tysne"
+            };
+            string expected = "Tysne";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                LastName = "Ted"
+            };
+            string expected = "Ted";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
+
+   
+
+
---
+++ C# ObjectOrientedProgramming/ACM/Tests/ACM.BLTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ACM.BLTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("ACM.BLTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b030246b-bd4e-495a-88ea-2ee725071fe0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
