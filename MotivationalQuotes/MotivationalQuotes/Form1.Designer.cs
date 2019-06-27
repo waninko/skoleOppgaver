80a2bdb0c990ff0df47d55af158b535f2ffe1e9a
waninko <43968374+waninko@users.noreply.github.com>
2019-06-27 13:30:57 +0200

    motivational quotes - C#

---
+++ MotivationalQuotes/MotivationalQuotes/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace MotivationalQuotes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textView = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textView.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textView
+            // 
+            this.textView.BackColor = System.Drawing.Color.Transparent;
+            this.textView.Controls.Add(this.label1);
+            this.textView.ForeColor = System.Drawing.Color.White;
+            this.textView.Location = new System.Drawing.Point(27, 115);
+            this.textView.Name = "textView";
+            this.textView.Size = new System.Drawing.Size(471, 241);
+            this.textView.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Gold;
+            this.label2.Location = new System.Drawing.Point(140, 592);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Black;
+            this.listBox1.ForeColor = System.Drawing.Color.White;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(42, 397);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(430, 164);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.viewQuotes);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(272, 590);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.browseFile);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(132, 567);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(959, 687);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textView);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Motivational Quotes!";
+            this.textView.ResumeLayout(false);
+            this.textView.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel textView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ MotivationalQuotes/MotivationalQuotes/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; //For å lese .txt fil
+using System.Drawing.Text;
+using System.Net.Mime;
+
+//for å assigne custom font
+
+namespace MotivationalQuotes
+{
+    public partial class Form1 : Form
+    {
+        int totalLines; //For å se hvor mange linjer som er lasta inn
+        PrivateFontCollection pfc = new PrivateFontCollection(); //For å bruke custom font
+
+        public Form1()
+        {
+            InitializeComponent();
+            pfc.AddFontFile("Text.ttf"); //Finne og laste inn fonten
+        }
+
+        private void browseFile(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            //default location for dialogbox
+            openFileDialog1.InitialDirectory = @"C:\";
+            //dialogbox tittel
+            openFileDialog1.Title = "Browse Text Files";
+
+            //både fil og sti må finnes i systemet
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+
+            //sette opp filtype - her kun .txt 
+            openFileDialog1.DefaultExt = "txt";
+            openFileDialog1.Filter = "Text files (*.txt)|*.txt";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            //sette opp til readonly
+            openFileDialog1.ReadOnlyChecked = true;
+            openFileDialog1.ShowReadOnly = true;
+
+            //om fil er selected og OK er trykket
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //sett inn fil navn & sti i dialogbox
+                textBox1.Text = openFileDialog1.FileName;
+                //tømme info i ListBox
+                listBox1.Items.Clear();
+                //lese fil
+                readFile();
+            }
+
+        }
+
+        private void viewQuotes(object sender, EventArgs e) //er koblet til selected item via EventArgs - tittel i label1
+        {
+            //still labelwidth i forhold til quote - unngå overlapping i view'et
+            label1.MaximumSize = new Size(textView.Width + 5,0);
+            label1.AutoSize = true;
+
+            //kliss på ny font og still størrelse & stil
+            label1.Font = new Font(pfc.Families[0], 20, FontStyle.Bold);
+
+            //vis selecta item i labelen (tittel)
+            label1.Text = listBox1.SelectedItem.ToString();
+        }
+
+
+        private void readFile() //last inn text fra fil i label2
+        {
+            string[] lines; //array til å holde quotes
+            var list = new List<string>(); 
+
+            //åpne og les fil
+            var fileStream = new FileStream(textBox1.Text, FileMode.Open, FileAccess.Read);
+
+            //loop gjennom og legg inn linjer quotes i liste og push i array
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                //sett readline til line sålenge den ikke er tom
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    //legg isåfall (den ikke-tomme) line til i lista
+                    list.Add(line);
+                    //legg til én til på total lines telleren
+                    totalLines++;
+                }
+
+                lines = list.ToArray(); //gjør om til array
+
+                //loop gjennom antall linjer i lines
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    //legg til en og en i listBox
+                    listBox1.Items.Add(lines[i]);
+                }
+                //vis hvor mange quotes som er lasta inn
+                label2.Text = "Total Lines Loaded " + totalLines;
+            }
+        } 
+    }
+}
